69a33b54650ccada11a8b35cd6ba64544d704f7f
Valerio Bigiani <vbigiani@gmail.com>
2026-08-07 22:41:20 +0200

    Actual Greywolf allies in cutscene

---
+++ compile/prism.d
@@ -0,0 +1,8 @@
+REPLACE_TRANS_ACTION prism BEGIN 4 END BEGIN END
+~CreateCreature(\"GREYWO\",\[3831.1829\],S)~
+~CreateCreature("GREYWO",[3831.1829],S)
+CreateCreature("tb#gwf01",[3831.1829],S)
+CreateCreature("tb#gwf02",[3831.1829],S)
+CreateCreature("tb#gwf03",[3831.1829],S)
+CreateCreature("tb#gwf04",[3831.1829],S)
+~
